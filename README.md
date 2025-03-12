c7c9d5f1d9aaec5a6772539d47c9cf638524f68e
JazimLatif <74251072+JazimLatif@users.noreply.github.com>
2025-03-12 03:45:34 +0000

    uncommenting my test for viewmodel and updating readme

--- README.md
+++ README.md
@@ -5,9 +5,10 @@ Thanks for taking the time to review my submission for the Built by Pixel Androi
 This was a fun project that expanded upon my experience and I had a lot of fun making it.
 
 I attempted to write clean, SOLID code, demonstrating separation of concerns, clean architecture and sound MVVM design decisions.  
-The project is scalable, I included a navigation component in case more screens need to be added  
 
-I chose the minSdk version to be 26 since the Times news app supports Android 8.0 and up.
+The project is scalable, I included a navigation component in case more screens need to be added.
+
+I chose the minSdk version to be 26 since the Times app supports Android 8.0 and up.
 
 Different UI states were considered by my use of `rememberSaveable`, using just `remember` would have lost a coin selection on orientation change
 
@@ -18,10 +19,12 @@ I attempted to animate the list by using a scroll to top button, which smoothly 
 ### Improvements:
 
 - I would like to get my ViewModel Tests working, currently they fail due to an error which I spent a long time trying to debug
+- For this reason, the test (even the alphabetisation test)
 - A filter/ search seems appropriate, the best coins to test with were the higher ranked ones (most of these had an image and description associated with them)
 whereas some of the coins at the top of the alphabetised list don't
 - I think perhaps the BottomSheet component would be more testable if it took more parameters to hoist state better (like passing the list of coins to the composable)
-
+- The sorting by name alphabetically is working, however some coin names start with a space (eg: " Bakkt Holdings, Inc."). I didn't want to alter the data and logic of kotlin's sorting function,
+although the brief said sorted alphabetically by name, I wasn't sure what to do here so I left it as-is.
 ---
 
-## Thanks again
+## Thanks again!
--- app/src/test/java/com/jazim/pixelnews/ViewModelTest.kt
+++ app/src/test/java/com/jazim/pixelnews/ViewModelTest.kt
@@ -1,95 +1,135 @@
-//package com.jazim.pixelnews
-//
-//import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-//import com.jazim.pixelnews.domain.model.ShortCoin
-//import com.jazim.pixelnews.domain.repository.CoinRepository
-//import com.jazim.pixelnews.presentation.coins.CoinViewModel
-//import com.jazim.pixelnews.presentation.state.ShortCoinState
-//import io.mockk.MockKAnnotations
-//import io.mockk.coEvery
-//import io.mockk.every
-//import io.mockk.mockk
-//import kotlinx.coroutines.Dispatchers
-//import kotlinx.coroutines.ExperimentalCoroutinesApi
-//import kotlinx.coroutines.test.TestDispatcher
-//import kotlinx.coroutines.test.UnconfinedTestDispatcher
-//import kotlinx.coroutines.test.advanceUntilIdle
-//import kotlinx.coroutines.test.resetMain
-//import kotlinx.coroutines.test.runTest
-//import kotlinx.coroutines.test.setMain
-//import org.junit.After
-//import org.junit.Assert.assertEquals
-//import org.junit.Assert.assertNull
-//import org.junit.Before
-//import org.junit.Rule
-//import org.junit.Test
-//import org.junit.rules.TestWatcher
-//import org.junit.runner.Description
-//
-///**
-// * Example local unit test, which will execute on the development machine (host).
-// *
-// * See [testing documentation](http://d.android.com/tools/testing).
-// */
-//
-//@ExperimentalCoroutinesApi
-//class CoinViewModelTest {
-//
-//    @get:Rule
-//    val instantExecutorRule = InstantTaskExecutorRule()
-//
-//    private lateinit var coinViewModel: CoinViewModel
-//    private lateinit var coinRepository: CoinRepository
-//
-//    @Before
-//    fun setup() {
-//        Dispatchers.setMain(UnconfinedTestDispatcher())
-//
-//        coinRepository = mockk(relaxed = true)
-//        coinViewModel = CoinViewModel(coinRepository)
-//    }
-//
-//    @After
-//    fun tearDown() {
-//        Dispatchers.resetMain()
-//    }
-//
-//    @Test
-//    fun whenGetCoinsSucceedsReturnsSuccess() = runTest(UnconfinedTestDispatcher()) {
-//
-//        val mockResponse = listOf(ShortCoin("1","foobar","baz"), ShortCoin("2","barfoo","bazeggs"))
-//        coEvery { coinRepository.getCoins() } returns Result.success(mockResponse)
-//
-//        coinViewModel.getAllCoins()
-//
-//        assertEquals(false, coinViewModel.allCoinsState.value.loading)
-//        assertNull(coinViewModel.allCoinsState.value.error)
-//
-//        val expectedState = listOf(ShortCoinState("1","foobar","baz"), ShortCoinState("2","barfoo","bazeggs"))
-//        assertEquals(expectedState, coinViewModel.allCoinsState.value.coins)
-//
-//    }
-//
-//    @Test
-//    fun whenGetCoinsFailsReturnsFailure() = runTest(UnconfinedTestDispatcher()) {
-//        val errorMessage = "Failed to load coins"
-//        coEvery { coinRepository.getCoins() } returns Result.failure(Exception(errorMessage))
-//
-//        coinViewModel.getAllCoins()
-//        advanceUntilIdle()
-//
-//        assertEquals(false, coinViewModel.allCoinsState.value.loading)
-//        assertEquals(errorMessage, coinViewModel.allCoinsState.value.error)
-//        assertEquals(emptyList<String>(), coinViewModel.allCoinsState.value.coins)
-//    }
-//
-//    @Test
-//    fun coinsAreSortedAlphabetically() = runTest(UnconfinedTestDispatcher()) {
-//        val mockResponse = listOf(ShortCoin("1","beta","b"), ShortCoin("2","alpha","a"))
-//        coEvery { coinRepository.getCoins() } returns Result.success(mockResponse)
-//
-//        val alphabeticallySortedCoins = coinViewModel.getCoinsAlphabetically()
-//
-//        assertEquals(alphabeticallySortedCoins, listOf(ShortCoinState("2","alpha","a"), ShortCoinState("1","beta","b")))
-//    }
-//}
+package com.jazim.pixelnews
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.jazim.pixelnews.domain.model.ShortCoin
+import com.jazim.pixelnews.domain.repository.CoinRepository
+import com.jazim.pixelnews.presentation.coins.CoinViewModel
+import com.jazim.pixelnews.presentation.state.ShortCoinState
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+
+@ExperimentalCoroutinesApi
+class CoinViewModelTest {
+
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var coinViewModel: CoinViewModel
+    private lateinit var coinRepository: CoinRepository
+
+    @Before
+    fun setup() {
+        Dispatchers.setMain(UnconfinedTestDispatcher())
+
+        coinRepository = mockk(relaxed = true)
+        coinViewModel = CoinViewModel(coinRepository)
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+
+    @Test
+    fun whenGetCoinsSucceedsReturnsSuccess() = runTest(UnconfinedTestDispatcher()) {
+
+        val mockResponse = listOf(ShortCoin("1","foobar","baz"), ShortCoin("2","barfoo","bazeggs"))
+        coEvery { coinRepository.getCoins() } returns Result.success(mockResponse)
+
+        coinViewModel.getAllCoins()
+
+        assertEquals(false, coinViewModel.allCoinsState.value.loading)
+        assertNull(coinViewModel.allCoinsState.value.error)
+
+        val expectedState = listOf(ShortCoinState("1","foobar","baz"), ShortCoinState("2","barfoo","bazeggs"))
+        assertEquals(expectedState, coinViewModel.allCoinsState.value.coins)
+
+    }
+
+    @Test
+    fun whenGetCoinsFailsReturnsFailure() = runTest(UnconfinedTestDispatcher()) {
+        val errorMessage = "Failed to load coins"
+        coEvery { coinRepository.getCoins() } returns Result.failure(Exception(errorMessage))
+
+        coinViewModel.getAllCoins()
+        advanceUntilIdle()
+
+        assertEquals(false, coinViewModel.allCoinsState.value.loading)
+        assertEquals(errorMessage, coinViewModel.allCoinsState.value.error)
+        assertEquals(emptyList<String>(), coinViewModel.allCoinsState.value.coins)
+    }
+
+    @Test
+    fun coinsAreSortedAlphabetically() = runTest(UnconfinedTestDispatcher()) {
+        val mockResponse = listOf(
+            ShortCoin("1", "beta", "b"),
+            ShortCoin("2", " alpha", "a"), // with a space at the start
+            ShortCoin("3", "delta", "d"),
+            ShortCoin("4", "gamma", "g"),
+            ShortCoin("5", "epsilon", "e"),
+            ShortCoin("6", "zeta", "z"),
+            ShortCoin("7", "\$symbol", "!"),
+            ShortCoin("8", "eta", "h"),
+            ShortCoin("9", "iota", "i"),
+            ShortCoin("10", "kappa", "k"),
+            ShortCoin("11", "1hash", "1"),
+            ShortCoin("12", " omega", "o"), // another leading space
+            ShortCoin("13", "@at", "@"),
+            ShortCoin("14", "theta", "t"),
+            ShortCoin("15", "APPLE", "A"),
+            ShortCoin("17", "apple", "a"),
+            ShortCoin("16", "GARY", "G")
+        )
+        coEvery { coinRepository.getCoins() } returns Result.success(mockResponse)
+
+        val alphabeticallySortedCoins = coinViewModel.getCoinsAlphabetically()
+
+        assertEquals(
+            alphabeticallySortedCoins,
+            listOf(
+                ShortCoin("2", " alpha", "a"),
+                ShortCoin("12", " omega", "o"),
+                ShortCoin("7", "\$symbol", "!"),
+                ShortCoin("11", "1hash", "1"),
+                ShortCoin("15", "APPLE", "A"),
+                ShortCoin("16", "GARY", "G"),
+                ShortCoin("17", "apple", "a"),
+                ShortCoin("13", "@at", "@"),
+                ShortCoin("1", "beta", "b"),
+                ShortCoin("3", "delta", "d"),
+                ShortCoin("5", "epsilon", "e"),
+                ShortCoin("8", "eta", "h"),
+                ShortCoin("4", "gamma", "g"),
+                ShortCoin("9", "iota", "i"),
+                ShortCoin("10", "kappa", "k"),
+                ShortCoin("14", "theta", "t"),
+                ShortCoin("6", "zeta", "z")
+            )
+        )
+
+    }
+}
